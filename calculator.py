9cae7129e0bbd09367e6e88bf053d23010a17946
marybahati <bahatimary745@gmail.com>
2019-04-05 21:11:07 +0300

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,28 @@
+def add(a,b):
+	c=a+b
+	print(c)
+
+
+def subtract(c,d):
+		e=c-d
+		print(e)
+
+def multiply(a,b):
+	    f=a*b
+	    print(f)
+def division(a,b):
+	    f=a/b
+	    print(f)
+def modulus(a,b):
+	    f=a%b
+	    print(f)
+def exponet(a,b):
+	    g=a**b
+	    print(g)
+def whole(a,b):
+	    f=a//b
+	    print(f)
+def percentage(a,b):
+	c=(a/b)*100
+	print(c)
+	
